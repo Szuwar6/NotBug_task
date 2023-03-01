decc5fcabc2ada66c6999191351c17a7707e5720
Marek <marekszubartowicz@gmail.com>
2023-03-01 17:43:15 +0100

    refactor

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-users = [{"name": "Kamil", "country": "Poland"}, {"name": "John", "country": "USA"},
-         {"name": "Yeti", "country": "USA"}]
-
-
 def only_poland(list):
     return [i for i in list if i.get("country") == "Poland"]
-
-
-print(only_poland(users))
-
-numbers = [1, 5, 2, 3, 1, 4, 1, 23, 12, 2, 3, 1, 2, 31, 23, 1, 2, 3, 1, 23, 1, 2, 3, 123]
 
 
 def sum_of_first_ten_elements(list):
@@ -17,11 +8,18 @@ def sum_of_first_ten_elements(list):
     return sum(new_list)
 
 
-print(sum_of_first_ten_elements(numbers))
-
-
 def power_of_2(n):
     return [2 ** n for n in range(1, n)]
 
 
-print(power_of_2(20))
+if __name__ == "__main__":
+    users = [{"name": "Kamil", "country": "Poland"}, {"name": "John", "country": "USA"},
+             {"name": "Yeti", "country": "USA"}]
+
+    print(only_poland(users))
+
+    numbers = [1, 5, 2, 3, 1, 4, 1, 23, 12, 2, 3, 1, 2, 31, 23, 1, 2, 3, 1, 23, 1, 2, 3, 123]
+
+    print(sum_of_first_ten_elements(numbers))
+
+    print(power_of_2(20))
